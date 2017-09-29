6984cb58b14913e2b9b8be438e7ff2824a0b9d31
hitswint <wgq_hit@126.com>
2017-09-29 14:22:18 +0800

    Fixes.

--- config/start_gunicorn.sh
+++ config/start_gunicorn.sh
@@ -1,2 +1,4 @@
+#! /bin/bash
+
 # nohup gunicorn --chdir /home/swint/git-repo/vmaig_blog/ --bind unix:/tmp/vmaig_blog.socket vmaig_blog.wsgi:application --reload&
-nohup gunicorn --chdir /home/swint/git-repo/vmaig_blog --bind 127.0.0.1:8000 vmaig_blog.wsgi:application --reload&
+/bin/bash -c "source /home/swint/.virtualenvs/py3/bin/activate; gunicorn --chdir /home/swint/git-repo/vmaig_blog --bind 127.0.0.1:8000 vmaig_blog.wsgi:application --reload; exec /bin/bash -i"
